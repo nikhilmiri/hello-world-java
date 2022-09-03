c03d584a4d7a4c97919d70141ad77e190266c508
nikhilmiri <112744897+nikhilmiri@users.noreply.github.com>
2022-09-03 15:09:12 +0530

    Update jcalculator.java

--- jcalculator.java
+++ jcalculator.java
@@ -2,12 +2,12 @@ public class jcalculator {
   public static void main(String[] args) {
      int a =20,b=5;
       
-      System.out.println("additon of two num : " (a+b));
+      System.out.println("additon of two num : " + (a+b));
       
-      System.out.println("subtract of two num : " (a-b));
+      System.out.println("subtract of two num : " + (a-b));
       
-      System.out.println("multiplication of two num : " (a*b));
+      System.out.println("multiplication of two num : " + (a*b));
       
-      System.out.println("division of two num : " (a/b));
+      System.out.println("division of two num : " + (a/b));
   }
 }
