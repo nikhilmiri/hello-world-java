0aa2a0bc457728266167f9b93559767e52f0628c
nikhilmiri <112744897+nikhilmiri@users.noreply.github.com>
2022-09-03 15:05:36 +0530

    Update and rename HelloWorld.java to jcalculator.java

---
+++ jcalculator.java
@@ -0,0 +1,13 @@
+public class jcalculator {
+  public static void main(String[] args) {
+     int a =20,b=5;
+      
+      System.out.println("additon of two num : " (a+b));
+      
+      System.out.println("subtract of two num : " (a-b));
+      
+      System.out.println("multiplication of two num : " (a*b));
+      
+      System.out.println("division of two num : " (a/b));
+  }
+}
